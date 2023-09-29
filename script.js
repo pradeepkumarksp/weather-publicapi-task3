790f7af53eb8e486e17315f728f4aeaa4cfa4cf7
pradeepkumarksp <mycomputer252545@gmail.com>
2023-09-29 15:13:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// create div container
+var div=document.createElement('div');
+div.style.textAlign='center';
+// create input element
+var input=document.createElement('input');
+input.setAttribute('type','text');
+input.setAttribute('id','city');
+input.setAttribute('placeholder','Enter Country name here...');
+
+// crreate button element
+var button=document.createElement('button');
+button.setAttribute('type','button');
+button.innerHTML='Get Weather';
+
+// add event to button
+button.addEventListener('click',getWeather);
+
+// create div element for out put handling
+
+let temp=document.createElement('div');
+temp.setAttribute('id','temp');
+let wind=document.createElement('div');
+wind.setAttribute('id','wind');
+let description=document.createElement('div');
+description.setAttribute('id','description');
+div.append(input,button,temp,wind,description);
+document.body.append(div);
+
+async function getWeather(){
+    try{
+    let weather=document.getElementById('city').value;   
+    let url=`https://goweather.herokuapp.com/weather/${weather}`;   
+
+    let res=await fetch(url);  
+    let res1=await res.json();     
+
+    console.log(res1.temperature);   
+    temp.innerHTML=`Temperature: ${res1.temperature}`;
+    console.log(res1.wind);   
+    wind.innerHTML=`Wind: ${res1.wind}`;
+    console.log(res1.description);   
+    description.innerHTML=`Description: ${res1.description}`;
+    
+        
+    }
+    catch(error){
+        console.log('Weather not found!!!')
+    }
+}
